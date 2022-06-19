d7744253d3118c94d3088088521d135f52d1cbd7
Evgenii Klimov <eklmv.spareacc@gmail.com>
2022-06-19 14:30:40 +0300

    Less test

--- students_test.go
+++ students_test.go
@@ -3,6 +3,7 @@ package coverage
 import (
 	"os"
 	"testing"
+	"time"
 )
 
 // DO NOT EDIT THIS FUNCTION
@@ -32,6 +33,78 @@ func Test_Len(t *testing.T) {
 			got := tcase.p.Len()
 			if got != tcase.expected {
 				t.Errorf("[%s] expected: %d, got: %d", name, tcase.expected, got)
+			}
+		})
+	}
+}
+
+func Test_Less(t *testing.T) {
+	tData := map[string]struct {
+		p People
+		expected bool
+	}{
+		"equal": 
+			{
+				People{
+						{"Aaron", "Aaronson", time.Time{}},
+						{"Aaron", "Aaronson", time.Time{}},
+					},
+					false,
+			},
+		"only different last name, where 0 < 1":
+			{
+				People{
+					{"Aaron", "Aaronson", time.Time{}},
+					{"Aaron", "Baronson", time.Time{}},
+				},
+				true,
+			},
+		"only different last name, where 0 > 1":
+			{
+				People{
+					{"Aaron", "Caronson", time.Time{}},
+					{"Aaron", "Aaronson", time.Time{}},
+				},
+				false,
+			},	
+		"only different first name, where 0 < 1":
+			{
+				People{
+					{"Aaron", "Aaronson", time.Time{}},
+					{"Baron", "Aaronson", time.Time{}},
+				},
+				true,
+			},	
+		"only different first name, where 0 > 1":
+			{
+				People{
+					{"Caron", "Aaronson", time.Time{}},
+					{"Aaron", "Aaronson", time.Time{}},
+				},
+				false,
+			},
+		"only different birthday, where 0 < 1":
+			{
+				People{
+					{"Aaron", "Aaronson", time.Time{}},
+					{"Aaron", "Aaronson", time.Time{}.Add(time.Hour * 10)},
+				},
+				false,
+			},
+		"only different birthday, where 0 > 1":
+			{
+				People{
+					{"Aaron", "Aaronson", time.Time{}.Add(time.Hour * 10)},
+					{"Aaron", "Aaronson", time.Time{}},
+				},
+				true,
+			},
+	}
+	for name, tcase := range tData {
+		t.Run(name, func(t *testing.T) {
+			got := tcase.p.Less(0, 1)
+			if got != tcase.expected {
+				t.Errorf("[%s] expected: %t, got: %t", name, tcase.expected, got)
 			}
 		})
 	}
